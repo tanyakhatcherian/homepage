8fbcb48f9e285aa6feea44bcd9cc834daffbbc37
tanyakhatcherian <tanyakhatcherian@hotmail.ca>
2021-04-18 18:46:45 -0400

    april18th-commit-3

--- script.js
+++ script.js
@@ -36,23 +36,21 @@ function slideshow() {
 }
 
 // filtering for teas + sale
-// the product filter is working, but the caffiene filter is faulty for example if i click "green, low" it should show the item that is green tea with low caffiene, but it doesn't it shows both green teas, it only works with high level caffiene which i see is whats being called in the function but i don't know how to apply it to all filters of caffiene level. i tried changing value=caffiene instead of value=high in html and jscript but it didn't make a difference, can you help me troubleshoot? if i can fix the jquery it'll be fixed on the teas html as well. further, can i copy+paste this function again below and change the variable names to work with my teaware page? 
+// the product filter is working, but the caffiene filter is faulty for example if i click "green, low" it should show the item that is green tea with low caffiene, but it doesn't it shows both green teas, it only works with caffiene-free caffiene which i see is whats being called in the function but i don't know how to apply it to all filters of caffiene level. can you help me troubleshoot? i think whats happening is that as soon as the first condition is met it stops which is why caffiene free is showing first.. so how do i fix it? if i can fix the jquery it'll be fixed on the teas html as well. further, can i copy+paste this function again below and change the variable names to work with my teaware page? 
 
 $('input[type="checkbox"]').click(function() {
     // Cache These Variables
     var $checked = $('input[type="checkbox"]:checked');
     var $productsDiv = $('.products > div');
-    var $caffieneCheckBox = $('input[type="checkbox"][value="high"]');
+    var $caffieneCheckBox = $('input[type="checkbox"][value="level"]');
 
     if ($checked.length > 0) {
         $productsDiv.hide();
-        // If only high Checkbox is Checked
         if ($checked.length == 1 && $caffieneCheckBox.is(':checked')) {
               $('.products > div[data-caffiene=' + $caffieneCheckBox.attr('id') + ']').show();
         } 
         // If Others are also Checked
         else {
-            // All Checkboxes and Not high Checkbox
             $checked.not($caffieneCheckBox).each(function() {
                 var dataCaffiene = '';
                 if ($caffieneCheckBox.is(':checked')) {
